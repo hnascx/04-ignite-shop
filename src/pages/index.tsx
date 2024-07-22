b0ee4fc896bc96724b716ffb1168149c6fc6960f
hnascx <henriquehusky@gmail.com>
2024-07-22 17:45:51 -0300

    feat/Configuring Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat'
+})
+
 export default function Home() {
   return (
-    <h1>Hello fuckin' WORLD!</h1>
+    <Button>Go shawty</Button>
   )
 }
