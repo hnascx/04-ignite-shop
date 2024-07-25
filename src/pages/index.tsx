cf666e383daf302e7879f0108b6b4bf7e5ab66f0
hnascx <henriquehusky@gmail.com>
2024-07-25 10:07:41 -0300

    feat/Configuring Stripe account

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { HomeContainer, Product } from "../styles/pages/home";
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
+import camiseta4 from '../assets/camisetas/4.png'
 
 import 'keen-slider/keen-slider.min.css'
 
@@ -48,11 +49,11 @@ export default function Home() {
       </Product>
 
       <Product className="keen-slider__slide">
-        <Image src={camiseta3} width={520} height={480} alt="" />
+        <Image src={camiseta4} width={520} height={480} alt="" />
 
         <footer>
-          <strong>Camiseta Z</strong>
-          <span>R$ 99,90</span>
+          <strong>Camiseta S</strong>
+          <span>R$ 109,90</span>
         </footer>
       </Product>
     </HomeContainer>
