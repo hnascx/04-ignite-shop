1155b2b62e9964f6d64aabdebbe8fc60d55355d6
hnascx <henriquehusky@gmail.com>
2024-07-25 18:11:06 -0300

    feat/Fetching products from Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,20 @@ import camiseta3 from '../assets/camisetas/3.png'
 import camiseta4 from '../assets/camisetas/4.png'
 
 import 'keen-slider/keen-slider.min.css'
+import { stripe } from "../lib/stripe";
+import { GetServerSideProps } from "next";
+import Stripe from "stripe";
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
   const [ sliderRef ] = useKeenSlider({
     slides: {
       perView: 3,
@@ -21,6 +33,8 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(products)}</pre>
+
       <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
@@ -58,4 +72,31 @@ export default function Home() {
       </Product>
     </HomeContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  console.log(response.data);
+  
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount / 100,
+    }
+  })
+  
+
+  return {
+    props: {
+      products,
+    }
+  }
 }
