6af5f5819510d5c4ee2c753f237cf5a4ad8d485e
hnascx <henriquehusky@gmail.com>
2024-07-31 11:24:21 -0300

    feat/Product page visual structure done

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,23 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <div>
-      <div>Product: {JSON.stringify(query)}</div>
-    </div>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, accusamus nesciunt. Quibusdam, blanditiis quisquam ut odio perferendis, repellendus vitae cumque consectetur, dignissimos distinctio nemo? Officiis, quaerat sint?</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
