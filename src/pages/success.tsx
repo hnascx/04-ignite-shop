f249da84fc865ff39d5bbc3cd604bbb0d6d5d1da
hnascx <henriquehusky@gmail.com>
2024-08-01 13:18:35 -0300

    feat/Visual structure of success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import { ImageContainer } from "../styles/pages/success";
+import { SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <div>
-      <div>Success</div>
-    </div>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Henrique Xavier</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
