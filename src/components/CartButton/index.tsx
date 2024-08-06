775f0475236a94fe25309cf021454da3de6345e7
hnascx <henriquehusky@gmail.com>
2024-08-05 22:27:03 -0300

    feat/Cart button added on header

---
+++ src/components/CartButton/index.tsx
@@ -0,0 +1,10 @@
+import { Handbag } from "phosphor-react";
+import { CardButtonContainer } from "./styles";
+
+export function CartButton() {
+  return (
+    <CardButtonContainer>
+      <Handbag weight="bold" />
+    </CardButtonContainer>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,14 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
+import { Container } from "../styles/pages/app"
+import { Header } from "../components/Header"
 
 globalStyles()
-
-import logoImg from '../assets/logo.svg'
-import { Container, Header } from "../styles/pages/app"
-
-import Image from 'next/image'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
-      <Header>
-        <Image src={logoImg} alt="" />
-      </Header>
-
+      <Header />
       <Component {...pageProps} />
     </Container>
   )
