06f2d177f96c1bc755fea80081a70f20d29d3ef6
hnascx <henriquehusky@gmail.com>
2024-09-06 17:23:24 -0300

    feat/CartContext done

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -13,6 +13,8 @@ export interface IProduct {
 
 interface CartContextData {
   cartItems: IProduct[]
+  addToCart: (product: IProduct) => void;
+  checkIfItemAlreadyExists: (productId: string) => boolean;
 }
 
 interface CartContextProviderProps {
@@ -24,8 +26,21 @@ export const CartContext = createContext({} as CartContextData)
 export function CartContextProvider({ children }: CartContextProviderProps) {
   const [cartItems, setCartItems] = useState<IProduct[]>([])
 
+  function addToCart(product: IProduct) {
+    setCartItems((state) => [...state, product])
+  }
+
+  function checkIfItemAlreadyExists(productId: string) {
+    return cartItems.some((product) => product.id === productId)
+  }
+
   return (
-    <CartContext.Provider value={{ cartItems }}>
+    <CartContext.Provider 
+      value={{ 
+        cartItems, 
+        addToCart, 
+        checkIfItemAlreadyExists 
+      }}>
       {children}
     </CartContext.Provider>
   )
