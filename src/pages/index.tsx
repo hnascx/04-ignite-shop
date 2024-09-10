cf3f5f45add8316ee39e0371beb61bcbb5c29ccc
hnascx <henriquehusky@gmail.com>
2024-09-10 17:46:38 -0300

    feat/Skeleton screen on Home done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,101 +1,136 @@
 import Image from "next/image";
-import Link from "next/link";
-import Head from 'next/head'
-import { GetStaticProps } from "next";
+import { HomeContainer, Product, SliderContainer } from "../styles/pages/home";
 
-import { useKeenSlider } from 'keen-slider/react'
+import { useKeenSlider } from "keen-slider/react";
 
+import "keen-slider/keen-slider.min.css";
 import { stripe } from "../lib/stripe";
-import { HomeContainer, Product } from "../styles/pages/home";
-
-import 'keen-slider/keen-slider.min.css'
+import { GetStaticProps } from "next";
 import Stripe from "stripe";
+import Link from "next/link";
+import Head from "next/head";
+
+import useEmblaCarousel from "embla-carousel-react";
+import { ProductSkeleton } from "../components/ProductSkeleton";
+import { MouseEvent, useEffect, useState } from "react";
 import { CartButton } from "../components/CartButton";
-import { useCart } from "../hooks/useCart";
 import { IProduct } from "../contexts/CartContext";
-import { MouseEvent } from "react";
+import { useCart } from "../hooks/useCart";
 
 interface HomeProps {
-  products: IProduct[]
+  products: IProduct[];
 }
 
 export default function Home({ products }: HomeProps) {
-  const [ sliderRef ] = useKeenSlider({
-    slides: {
-      perView: 3,
-      spacing: 48,
-    }
-  })
+  const [isLoading, setIsLoading] = useState(true);
 
-  const { addToCart, checkIfItemAlreadyExists } = useCart()
+  useEffect(() => {
+    // fake loading to use the skeleton loading from figma
+    const timeOut = setTimeout(() => setIsLoading(false), 2000);
 
-  function handleAddToCart(e: MouseEvent<HTMLButtonElement>, product: IProduct) {
-    e.preventDefault()
-    addToCart(product)
+    return () => clearTimeout(timeOut);
+  }, []);
+
+  const [emblaRef] = useEmblaCarousel({
+    align: "start",
+    skipSnaps: false,
+    dragFree: true,
+  });
+
+  const { addToCart, checkIfItemAlreadyExists } = useCart();
+
+  function handleAddToCart(
+    e: MouseEvent<HTMLButtonElement>,
+    product: IProduct
+  ) {
+    e.preventDefault();
+    addToCart(product);
   }
-  
+
   return (
     <>
       <Head>
         <title>Home | Ignite Shop</title>
       </Head>
 
-      <HomeContainer ref={sliderRef} className="keen-slider">
-        {products.map(product => {
-          return (
-            <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
-              <Product 
-                className="keen-slider__slide"
-              >
-                <Image src={product.imageUrl} width={520} height={480} alt="" />
+      <div style={{ overflow: "hidden", width: "100%" }}>
+        <HomeContainer>
+          <div className="embla" ref={emblaRef}>
+            <SliderContainer className="embla__container container">
+              {isLoading ? (
+                <>
+                  <ProductSkeleton className="embla__slide" />
+                  <ProductSkeleton className="embla__slide" />
+                  <ProductSkeleton className="embla__slide" />
+                </>
+              ) : (
+                <>
+                  {products.map((product) => (
+                    <Link
+                      key={product.id}
+                      href={`/product/${product.id}`}
+                      prefetch={false}
+                      passHref
+                    >
+                      <Product className="embla__slide">
+                        <Image
+                          src={product.imageUrl}
+                          width={520}
+                          height={480}
+                          alt=""
+                          placeholder="blur"
+                          blurDataURL={product.imageUrl}
+                        />
 
-                <footer>
-                  <div>
-                    <strong>{product.name}</strong>
-                    <span>{product.price}</span>
-                  </div>
-                  <CartButton 
-                    color="green" 
-                    size="large"
-                    disabled={checkIfItemAlreadyExists(product.id)}
-                    onClick={(e) => handleAddToCart(e, product)}
-                  />
-                </footer>
-              </Product>
-            </Link>
-          )
-        })}
-      </HomeContainer>
+                        <footer>
+                          <div>
+                            <strong>{product.name}</strong>
+                            <span>{product.price}</span>
+                          </div>
+                          <CartButton
+                            size="large"
+                            color="green"
+                            disabled={checkIfItemAlreadyExists(product.id)}
+                            onClick={(e) => handleAddToCart(e, product)}
+                          />
+                        </footer>
+                      </Product>
+                    </Link>
+                  ))}
+                </>
+              )}
+            </SliderContainer>
+          </div>
+        </HomeContainer>
+      </div>
     </>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
-    expand: ['data.default_price']
-  })
+    expand: ["data.default_price"],
+  });
 
-  const products = response.data.map(product => {
-    const price = product.default_price as Stripe.Price
-
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
     return {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: new Intl.NumberFormat('pt-br', {
-        style: 'currency',
-        currency: 'BRL'
+      price: new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
       }).format(price.unit_amount / 100),
       numberPrice: price.unit_amount / 100,
       defaultPriceId: price.id,
-    }
-  })
-  
+    };
+  });
 
   return {
     props: {
       products,
     },
-    revalidate: 60 * 60 * 2 // 2 hours
-  }
-}
+    revalidate: 60 * 60 * 2, // 2 hours,
+  };
+};
